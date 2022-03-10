c533dab07db828a0b421ee9655e168d4f8d4fc56
Albert Nguyen <ndttam.dev@gmail.com>
2022-03-10 22:07:07 +0700

    clear all to re-config again

--- src/utils/paths/index.tsx
+++
@@ -1,9 +0,0 @@
-export const PATH_ROOT = "/";
-export const CV_PAGE = "/cv-page";
-
-
-// Socials
-export const LINK_FACEBOOK = "https://www.facebook.com/tam.pi.33";
-export const LINK_LINKEDIN = "https://www.linkedin.com/in/tam-nguyen-6a1576183/";
-export const LINK_INSTAGRAM = "https://www.instagram.com/tam.pi.33/";
-export const LINK_GITHUB = "https://github.com/sr-albert/";
